61704d2aa35d5bca63c6b07ad6355abedbb4afe5
njflash88 <gordonchu88@yahoo.com>
2024-09-05 12:54:56 +0800

    Step 10. modify view and model

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from .models import Listing
+from datetime import timedelta
+from django.utils import timezone
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    # ! get all data from listing database
+    listings = Listing.objects.all()
+    # ! pass database records into listings context
+    context = {'listings':listings, 'date': timezone.now()}
+    return render(request, 'listings/listings.html', context )
 
 def listing(request):
     return render(request, 'listings/listing.html')
