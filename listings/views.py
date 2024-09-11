6eeacc030e67e1480fd56ed724aef76732d79ac6
njflash88 <gordonchu88@yahoo.com>
2024-09-11 10:29:12 +0800

    Step 15. modify Search page funtions

--- listings/views.py
+++ listings/views.py
@@ -3,6 +3,7 @@ from .models import Listing
 from datetime import timedelta
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator 
+from listings.choices import price_choices, bedroom_choices, district_choices
 
 # Create your views here.
 def index(request):
@@ -22,4 +23,37 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', {'listing':listing})
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+            
+    if 'title' in request.GET:
+        title = request.GET['title']
+        print(title)
+        if title:
+            queryset_list = queryset_list.filter(title__icontains=title)
+            
+    if 'district' in request.GET:
+        district = request.GET['district']
+        if district:
+            queryset_list = queryset_list.filter(district__iexact=district)        
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price) 
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms) 
+
+
+    context = {
+        'listings': queryset_list,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'district_choices': district_choices,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
